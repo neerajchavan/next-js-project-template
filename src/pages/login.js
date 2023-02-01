3b9221b88bb5ce1b8db4dfee7d11668fc2733a55
= <=>
2023-02-01 13:42:04 +0530

    fixed bootstrap cdn link issue

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <main className="flex-center vh-100">
+      <div className="container bg-white w-25 p-5">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary mt-4">
+            Submit
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
